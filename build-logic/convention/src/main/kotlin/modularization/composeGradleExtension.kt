55ce47b6021d0501881a4c252017a0646cdab6cf
mahdi <mahdiasd96@yahoo.com>
2024-07-12 18:24:46 +0330

    feat(dependencies): add shared dependencies module
    - Added a new shared dependencies module to centralize dependencies management.
    - Updated build.gradle to include the new shared dependencies module.

---
+++ build-logic/convention/src/main/kotlin/modularization/composeGradleExtension.kt
@@ -0,0 +1,37 @@
+package modularization
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.composeGradleExtension(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+    commonExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+
+        dependencies {
+
+            val bom = libs.findLibrary("androidx.compose.bom").get()
+            add("implementation", platform(bom))
+
+            add("implementation", libs.findLibrary("androidx.activity.compose").get())
+            add("implementation", libs.findLibrary("androidx.ui").get())
+            add("implementation", libs.findLibrary("androidx.ui.graphics").get())
+
+            add("implementation", libs.findLibrary("androidx.ui.tooling").get())
+            add("implementation", libs.findLibrary("androidx.ui.tooling.preview").get())
+            add("implementation", libs.findLibrary("androidx.material3").get())
+            add("implementation", libs.findLibrary("coil.compose").get())
+            add("implementation", libs.findLibrary("constraintlayout.compose").get())
+
+            add("debugImplementation", libs.findLibrary("androidx.ui.tooling").get())
+            add("debugImplementation", libs.findLibrary("androidx.ui.tooling.preview").get())
+            add("debugImplementation", libs.findLibrary("androidx.ui.test.manifest").get())
+        }
+
+    }
+
+}
