63251749a8bd7fa9591568f0493195e4e1608b5c
mahdi <mahdiasd96@yahoo.com>
2024-11-15 19:33:27 +0330

    feat(ads-detail): add pager indicator and image count to slider section
    - Implement `HorizontalPagerIndicator` composable to display page indicators for the image slider
    - Use `LazyRow` to render page indicators based on the current page and total page count
    - Highlight the current page indicator with a solid white color and reduce opacity for other indicators
    - Add image count and fullscreen icon overlaid on the image slider
    - Display the total number of images next to the fullscreen icon
    - Apply a semi-transparent black background to the image count and fullscreen icon for better visibility
    - Update `SliderSection` to include the `HorizontalPagerIndicator` and image count
    - Adjust imports and remove unused dependencies in the `ads-detail` module

--- data/src/main/java/com/divar/data/mapper/location/NeighborhoodMapper.kt
+++ data/src/main/java/com/divar/data/mapper/location/NeighborhoodMapper.kt
@@ -1,7 +1,7 @@
 package com.divar.data.mapper.location
 
 import com.divar.domain.model.location.Neighborhood
-import com.divar.network.dto.location.NeighborhoodResponse
+import com.divar.network.dto.neighborhood.NeighborhoodResponse
 
 fun NeighborhoodResponse.toDomain(): Neighborhood {
     return Neighborhood(id = id, name = name)
--- feature/ads-detail/src/main/java/com/divar/ads_detail/component/SliderSection.kt
+++ feature/ads-detail/src/main/java/com/divar/ads_detail/component/SliderSection.kt
@@ -1,39 +1,38 @@
 package com.divar.ads_detail.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.BookmarkBorder
+import androidx.compose.material.icons.filled.Fullscreen
 import androidx.compose.material.icons.filled.Print
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
 import com.divar.ads_detail.OnAction
 import com.divar.domain.fake_data.FakeData
 import com.divar.domain.model.ads.Ads
-import com.divar.domain.model.category.Category
-import com.divar.domain.model.location.Neighborhood
-import com.divar.domain.model.user.User
+import com.divar.ui.core.text.BodyMediumText
 import com.divar.ui.extension.baseModifier
 import com.divar.ui.them.AppTheme
 import com.divar.utils.coilRounded
@@ -44,7 +43,9 @@ fun SliderSection(
     ads: Ads,
     onAction: OnAction
 ) {
-    val pagerState = rememberPagerState { ads.images.size }
+    val pagerState = rememberPagerState {
+        ads.images.size
+    }
     Box(modifier = modifier)
     {
         HorizontalPager(state = pagerState) { pageIndex ->
@@ -99,9 +100,55 @@ fun SliderSection(
                 tint = AppTheme.colors.iconColor
             )
         }
+
+
+        Row(
+            modifier = Modifier.background(
+                color = Color.Black.copy(alpha = 0.4f),
+                shape = AppTheme.shapes.roundMedium
+            ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp, alignment = Alignment.CenterHorizontally)
+        ) {
+            Icon(
+                modifier = Modifier.size(24.dp),
+                imageVector = Icons.Default.Fullscreen,
+                contentDescription = ""
+            )
+            BodyMediumText(text = ads.images.size.toString())
+        }
+
+        HorizontalPagerIndicator(
+            modifier = Modifier.fillMaxWidth(),
+            pagerState
+        )
     }
 }
 
+@Composable
+fun HorizontalPagerIndicator(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState
+) {
+
+    LazyRow(modifier = modifier) {
+        items(pagerState.pageCount) { index ->
+            Spacer(
+                modifier = Modifier
+                    .then(
+                        if (index == pagerState.currentPage) {
+                            Modifier
+                                .background(color = Color.White, shape = CircleShape)
+                        } else {
+                            Modifier
+                                .background(color = Color.White.copy(alpha = 0.6f), shape = CircleShape)
+                        }
+                    )
+            )
+        }
+    }
+
+}
 
 @PreviewLightDark
 @Composable
